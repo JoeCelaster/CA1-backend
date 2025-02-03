13bfc8b7ba4776d0dbf5d5ed9be6e7c943f3ca0e
JoeCelaster <joecelaster2006@gmail.com>
2025-02-03 15:04:36 +0530

    i added

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express())
+
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.post("/login",(req,res)=>{
+    const {Username,email,password,Dob} = req.body
+
+    if (!Username){
+        res.status(400).json("Username cannot be empty")
+    }
+    if (!email){
+        res.status(400).json("Email cannot be empty")
+    }
+    if (password.length()<8 && password.length()>16){
+        res.status(400).json("Password length should be greater than 8 or less than or equal to 16")
+    }
+
+    else{
+        res.status(200).json(
+            {
+            "Username" : {Username},
+            "Email": {email},
+            "Password" : {password},
+            "Date of birth" : {Dob}
+            }
+        )
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running at port ${port}`)
+})
